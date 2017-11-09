6bbfed19ad08bf40f8a4f7f5122dac365096ffe9
Jun Kato <i@junkato.jp>
2017-11-09 19:50:51 +0900

    fix fatal typo bug

--- viz.js
+++ viz.js
@@ -10,15 +10,15 @@ var player;
 
 // APIの準備が出来ると呼ばれる
 window.onSongleWidgetAPIReady = function(SongleWidgetAPI){
-	window.SongleWidgetAPI = SongleWidgetAPI;
-	SongleWidget.System.defaultEndpointWebClientProtocol = "https:"; 
+	window.SW = SongleWidgetAPI;
+	SW.System.defaultEndpointWebClientProtocol = "https:"; 
 	init();
 }
 
 window.init = function(){
 	if ( getUrlVars().master == "1" ) {
 		// masterの場合 (引数 master=1)
-		player = new SongleWidgetAPI.Player({
+		player = new SW.Player({
 			mediaElement: "div#widget" // プレイヤーを埋め込むDOMを指定
 		});
 		player.accessToken = accessToken;
@@ -28,17 +28,17 @@ window.init = function(){
 	}
 	else {
 		// slaveの場合
-		player = new SongleWidgetAPI.Player();
+		player = new SW.Player();
 		player.accessToken = accessToken;
 	}
 
 	// slaveを同期させるプラグインを設定
-	player.addPlugin(new SongleWidget.Plugin.SongleSync());
+	player.addPlugin(new SW.Plugin.SongleSync());
 
 	// 利用するイベントのプラグインを設定
-	player.addPlugin(new SongleWidget.Plugin.Beat());
-	player.addPlugin(new SongleWidget.Plugin.Chorus());
-	player.addPlugin(new SongleWidget.Plugin.Chord());
+	player.addPlugin(new SW.Plugin.Beat());
+	player.addPlugin(new SW.Plugin.Chorus());
+	player.addPlugin(new SW.Plugin.Chord());
 
 	// 各イベントに対応するアクションを設定
 	setBeatEvent();
